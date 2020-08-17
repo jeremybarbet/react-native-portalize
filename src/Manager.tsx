23e97df95019098da7098ef92961aa2ae7414f02
Harvey Appleton <info@harveyappleton.co.uk>
2020-08-17 11:30:06 +0100

    Use UUID library to generate truly random IDs
    Resolves issue of hoped reliability from using Date.now() / a randomish number

--- src/Manager.tsx
+++ src/Manager.tsx
@@ -2,22 +2,22 @@ import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 export interface IManagerHandles {
-  mount(key: number, children: React.ReactNode): void;
-  update(key?: number, children?: React.ReactNode): void;
-  unmount(key?: number): void;
+  mount(key: string, children: React.ReactNode): void;
+  update(key?: string, children?: React.ReactNode): void;
+  unmount(key?: string): void;
 }
 
 export const Manager = React.forwardRef((_, ref): any => {
-  const [portals, setPortals] = React.useState<{ key: number; children: React.ReactNode }[]>([]);
+  const [portals, setPortals] = React.useState<{ key: string; children: React.ReactNode }[]>([]);
 
   React.useImperativeHandle(
     ref,
     (): IManagerHandles => ({
-      mount(key: number, children: React.ReactNode): void {
+      mount(key: string, children: React.ReactNode): void {
         setPortals(prev => [...prev, { key, children }]);
       },
 
-      update(key: number, children: React.ReactNode): void {
+      update(key: string, children: React.ReactNode): void {
         setPortals(prev =>
           prev.map(item => {
             if (item.key === key) {
@@ -29,7 +29,7 @@ export const Manager = React.forwardRef((_, ref): any => {
         );
       },
 
-      unmount(key: number): void {
+      unmount(key: string): void {
         setPortals(prev => prev.filter(item => item.key !== key));
       },
     }),
